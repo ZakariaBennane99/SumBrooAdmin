d30fbba45635c9defe13030be91fbb6f680e5afb
Zakaria Bennane <zakariamonre@gmail.com>
2023-10-27 00:37:32 +0300

    .

---
+++ src/pages/api/startingAnalytics.js
@@ -0,0 +1,17 @@
+/*
+
+here is the format your will be saving your data in:
+
+when the first 24H finishes, you should add the previous day's date
+not the current day
+analytics: {
+    data: [
+        {
+            date: 'FULL UTC DATE AT WHICH YOU TOOK THE DATA FROM PINTerst: THE DATE WILL BE IN THE FORMAT: YYYY-MM-DD',
+            reactions: 'UPDATED NUMBER OF REACTIONS BASED OFF THE FUNCTION',
+            comments: 'UPDATED NUMBER OF COMMENTS BASED OFF THE TOTAL REACTIONS'
+        }
+    ]
+}
+
+*/
